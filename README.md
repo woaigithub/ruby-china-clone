aa321896b058df5fb3a6e01d57241d19e9f8fbb9
woaigithub <jorden008@gmail.com>
2012-11-07 20:28:43 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ruby-china-clone
 ================
+online project management tools: https://pragmatic.ly/
+pomodoro tools: http://tomatoi.st/dktp
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyChinaClone::Application.config.secret_token = 'a9b8f049ccbd9085f425f5efad0f4d03f94b6a0176db91770357077d6c03a087d6af271c8bcab13d984a2ab266fb8367d6c109ec8843e42d4cbd882fe5f9bd7b'
