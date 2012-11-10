a5f1d9d4095af3dc26e73bf47eeff651c9bb1e25
woaigithub <jorden008@gmail.com>
2012-11-10 17:28:15 +0800

    sign up .....

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,20 @@
 class User < ActiveRecord::Base
- 
-  has_secure_password
+    
+
+#  has_secure_password
 
   attr_accessible :username, :email, :password, :password_confirmation
 
+  has_secure_password
+
   validates :username, :presence => true, :length => { :in =>6..20 }
   validates :email, :presence => true, :confirmation => true 
-  validates :password, :presence => true
-  validates :password_confirmation, :presence => true 
+#  validates :password, :presence => true
+#  validates :password_confirmation, :presence => true 
 
   has_many :topics
 
-  before_save :encrypt_password
 
-  private
   
-  def encrypt_password
-    
-  end
 
 end
