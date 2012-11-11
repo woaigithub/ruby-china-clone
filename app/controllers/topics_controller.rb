c4d7660069f77dbb6f24d4132a833036a01ab190
woaigithub <jorden008@gmail.com>
2012-11-11 22:55:55 +0800

    edit my topic ...

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,17 @@
 class TopicsController < ApplicationController
 
 
-  before_filter :require_signin, :only => [:new, :create]
+  before_filter :require_signin, :only => [:new, :create, :edit, :update, :destroy]
+  before_filter :require_owner, :only => [:edit, :update, :destroy]
 
+  def require_owner
+    @topic = Topic.find(params[:id])
+    unless @topic && current_user.id==@topic.user_id
+      flash[:notice]="you must be the topic's owner"
+      redirect_to topic_path @topic
+    end
+  end
+ 
   def require_signin
     unless signed_in?
       flash[:notice]="you should be sign in first!"
@@ -12,7 +21,7 @@ class TopicsController < ApplicationController
 
   def index
     page = params[:page]
-    @topics = Topic.paginate(:page=>page,:per_page=>5).includes(:node).includes(:user)
+    @topics = Topic.paginate(:page=>page,:per_page=>5).includes(:node).includes(:user).order("")
   end
 
 
@@ -51,5 +60,33 @@ class TopicsController < ApplicationController
     end
   end
 
+  def edit
+    @topic = Topic.find(params[:id])
+    @nodes = Node.all
+  end
 
+  def update
+    @node=Node.find(params[:topic][:node_id])
+    @topic=@node.topics.build(:title=>params[:title],
+                              :content => params[:content])
+    @topic.user_id=current_user.id
+
+    if @topic.save
+      flash[:notice]="update topic successful"
+      redirect_to topic_path @topic
+    else
+      @ndoes=Node.all
+      flash.now[:notice]="update topic fail"
+
+      render :edit
+    end
+  end
+  
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+
+    redirect_to root_path
+
+  end
 end
